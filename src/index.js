94ac41f6e063d61b1df1fefbbb996f9f84147f1c
Eli Lavoie <storey14@googlemail.com>
2021-02-08 14:21:03 -0600

    set index.js to use ApplicationView in BrowserRouter tags, set application view to render based on url, set / to be homepage, removed App.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom'
 import './index.css';
-import App from './App';
+import ApplicationView from './ApplicationView';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <ApplicationView />
+  </Router>,
   document.getElementById('root')
 );

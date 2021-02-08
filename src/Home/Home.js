090b42684704be2b3f052d8625b00d316fd3759a
Eli Lavoie <storey14@googlemail.com>
2021-02-08 15:15:40 -0600

    began work on homepage, added a div to simulate a navbar until navbar is created

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import '../css/mystyles.css'
-
+import './Home.css'
 const Home = () => {
     return(
-        <h1 className="title is-1">Hello World</h1>
+        <>
+            <div className="temp-nav-area"></div>
+            <div className="main">
+                <h1 className="title is-4 has-text-centered">Shopify Presents</h1>
+                <p className="title is-1 has-text-centered">The Shoppies</p>
+            </div>
+        </>
     )
 }
 

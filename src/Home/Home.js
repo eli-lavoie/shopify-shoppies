287b2cda8a4c21b175f59b53358549b0d3235ba4
Eli Lavoie <storey14@googlemail.comgit config --global user.name Eli>
2021-02-08 18:06:52 -0600

    minor homepage content and styling

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -8,6 +8,13 @@ const Home = () => {
             <div className="main">
                 <h1 className="title is-4 has-text-centered">Shopify Presents</h1>
                 <p className="title is-1 has-text-centered">The Shoppies</p>
+                <hr className="header-divider"/>
+                <div className="content has-text-centered">
+                    <h3>Shopify has expanded into the movie awards business, and we need your help!<br/> It's up to you to vote for your 5 favorite movies!</h3>
+                </div>
+                <div className="buttons has-addons is-centered">
+                    <button className="button is-primary is-light">Test</button>
+                </div>
             </div>
         </>
     )
